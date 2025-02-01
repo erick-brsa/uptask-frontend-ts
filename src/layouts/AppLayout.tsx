9d79399989e789c46ba9da56e4e19a5689462ce7
Erick Briones <erikalan518@gmail.com>
2025-02-01 17:54:54 -0600

    Creando el frontend

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from 'react-router-dom';
+import NavMenu from '@/components/NavMenu';
+import Logo from '@/components/Logo';
+
+export default function AppLayout() {
+	return (
+		<>
+			<header className="bg-gray-800 py-5">
+				<div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center px-10">
+					<div className="w-64">
+						<Logo />
+					</div>
+					
+					<NavMenu />
+				</div>
+			</header>
+
+			<section className="max-w-2xl mx-auto mt-10 p-5">
+				<Outlet />
+			</section>
+
+			<footer className="py-5">
+				<p className="text-center">
+					Todos los derechos reservados {new Date().getFullYear()}
+				</p>
+			</footer>
+		</>
+	);
+}
